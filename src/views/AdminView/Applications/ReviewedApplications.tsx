7181c66dd1f20ab896a8c3ebf7b5be0412e44f42
Zude Mwango <zudemwango@gmail.com>
2022-07-26 12:53:43 +0200

    ui clean up

--- src/views/AdminView/Applications/ReviewedApplications.tsx
+++ src/views/AdminView/Applications/ReviewedApplications.tsx
@@ -74,10 +74,7 @@ const ReviewedApplications: React.FC<React.PropsWithChildren<unknown>> = () => {
         filter: true,
         sort: false,
         customBodyRender: (value) => (
-          <Chip
-            sx={{ backgroundColor: '#0133a1', color: '#fff' }}
-            label={value}
-          />
+          <Chip variant="outlined" color="primary" label={value} />
         )
       }
     },
