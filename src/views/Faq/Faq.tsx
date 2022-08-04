24a7591fb5ace13773d5b9ed14ac367c7fe7fbab
alfeyo <alfeyokatebe@gmail.com>
2022-08-04 14:10:43 +0200

    added Accordion

--- src/views/Faq/Faq.tsx
+++ src/views/Faq/Faq.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import AppBar from '../hompage/Components/AppBar';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Header from '../hompage/Components/Header';
+import AppBar from '../hompage/Components/AppBar';
 
-function FAQ() {
+export default function FAQ() {
   return (
     <div>
       <AppBar />
       <Header />
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
-
-export default FAQ;
