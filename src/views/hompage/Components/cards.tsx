0717847dbce86186e1f4bc929a7b0e117e55bd36
Zude Mwango <zudemwango@gmail.com>
2023-07-11 23:16:03 +0200

    UI changes

--- src/views/hompage/Components/cards.tsx
+++ src/views/hompage/Components/cards.tsx
@@ -25,204 +25,119 @@ export default function Cards() {
         Why We Do This!
       </Typography>
       <Container fixed>
-        <Grid container>
-          <div className="challenge" style={{ marginBottom: '20px' }} />
-          <Grid
-            item
-            sm={12}
-            md={6}
-            lg={6}
-            xl={6}
-            sx={{ paddingRight: { xs: 0, sm: 0, md: '10px' } }}
-          >
-            <Box
-              sx={{
-                backgroundColor: '#F7F7F7',
-                padding: '20px'
-              }}
-            >
-              <div style={{ marginRight: '10px' }}>
-                <TaskAltOutlinedIcon
-                  sx={{
-                    float: 'left',
-                    width: '40px',
-                    height: '50px',
-                    color: '#EC008C',
-                    paddingRight: '3px',
-                    marginTop: {
-                      xs: '6px',
-                      sm: '10px',
-                      md: '7px'
-                    }
-                  }}
-                />
-              </div>
-
-              <Typography
-                variant="body2"
-                sx={{ padding: '10px', textAlign: 'left', marginLeft: '60px' }}
-              >
-                We believe that innovation can be the driving force behind
-                Stanbic Bank’s growth and success.
-              </Typography>
-            </Box>
-            <br />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
             <Box
               sx={{
                 backgroundColor: '#F7F7F7',
                 padding: '20px',
-                paddingBottom: '30px'
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: '20px'
               }}
             >
-              <div style={{ marginRight: '10px' }}>
-                <TaskAltOutlinedIcon
-                  sx={{
-                    float: 'left',
-                    width: '40px',
-                    height: '50px',
-                    color: '#EC008C',
-                    paddingRight: '3px',
-                    marginTop: {
-                      xs: '15px',
-                      sm: '10px',
-                      md: '5px'
-                    }
-                  }}
-                />
-              </div>
-
-              <Typography
-                variant="body2"
-                sx={{ padding: '10px', textAlign: 'left', marginLeft: '60px' }}
-              >
-                Your contributions here have the power to shape the future of
-                our organization and propel us to new heights..
+              <TaskAltOutlinedIcon
+                sx={{
+                  width: '40px',
+                  height: '50px',
+                  color: '#EC008C',
+                  marginRight: '10px'
+                }}
+              />
+              <Typography variant="body2">
+                We believe that innovation can be the driving force behind
+                Stanbic Bank’s growth and success.
               </Typography>
             </Box>
-            <br />
-            {/* <Box
-              sx={{
-                backgroundColor: '#F7F7F7',
-                padding: '20px'
-              }}
-            >
-              <div style={{ marginRight: '10px' }}>
-                <TaskAltOutlinedIcon
-                  sx={{
-                    float: 'left',
-                    width: '40px',
-                    height: '50px',
-                    color: '#EC008C',
-                    paddingRight: '2px',
-                    marginTop: {
-                      xs: '2px',
-                      sm: '10px',
-                      md: '-10px'
-                    }
-                  }}
-                />
-              </div>
-
-              <Typography
-                variant="body2"
-                sx={{ padding: '5px', textAlign: 'left', marginLeft: '60px' }}
-              >
-                Access to legal, accounting, financial planning and tech
-                support.
-              </Typography>
-            </Box> */}
-            <br />
           </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
             <Box
               sx={{
                 backgroundColor: '#F7F7F7',
-                padding: '20px'
+                padding: '20px',
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: '20px'
               }}
             >
-              <div style={{ marginRight: '10px' }}>
-                <TaskAltOutlinedIcon
-                  sx={{
-                    float: 'left',
-                    width: '40px',
-                    height: '50px',
-                    color: '#EC008C',
-                    paddingRight: '3px',
-                    marginTop: {
-                      xs: '-4px',
-                      sm: '10px',
-                      md: '7px'
-                    }
-                  }}
-                />
-              </div>
-
-              <Typography
-                variant="body2"
-                sx={{ padding: '10px', textAlign: 'left', marginLeft: '60px' }}
-              >
+              <TaskAltOutlinedIcon
+                sx={{
+                  width: '40px',
+                  height: '50px',
+                  color: '#EC008C',
+                  marginRight: '10px'
+                }}
+              />
+              <Typography variant="body2">
+                Your contributions here have the power to shape the future of
+                our organization and propel us to new heights.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <Box
+              sx={{
+                backgroundColor: '#F7F7F7',
+                padding: '20px',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              <TaskAltOutlinedIcon
+                sx={{
+                  width: '40px',
+                  height: '50px',
+                  color: '#EC008C',
+                  marginRight: '10px'
+                }}
+              />
+              <Typography variant="body2">
                 Whether you have a revolutionary product idea, a process
                 improvement suggestion, or a game-changing strategy, this portal
-                is the perfect space to unleash your creativity
+                is the perfect space to unleash your creativity.
               </Typography>
             </Box>
-            <br />
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
             <Box
               sx={{
                 backgroundColor: '#F7F7F7',
-                padding: '20px'
+                padding: '20px',
+                display: 'flex',
+                alignItems: 'center'
               }}
             >
-              <div style={{ marginRight: '10px' }}>
-                <TaskAltOutlinedIcon
-                  sx={{
-                    float: 'left',
-                    width: '40px',
-                    height: '50px',
-                    color: '#EC008C',
-                    paddingRight: '3px',
-                    marginTop: {
-                      xs: '35px',
-                      sm: '10px',
-                      md: '10px',
-                      lg: '10px',
-                      xl: '10px'
-                    }
-                  }}
-                />
-              </div>
-              <Typography
-                variant="body2"
+              <TaskAltOutlinedIcon
                 sx={{
-                  padding: '5px',
-                  textAlign: 'left',
-                  marginLeft: '60px'
+                  width: '40px',
+                  height: '50px',
+                  color: '#EC008C',
+                  marginRight: '10px'
                 }}
-              >
+              />
+              <Typography variant="body2">
                 We encourage all our internal staff to participate actively, as
                 your unique perspectives and expertise are invaluable to our
                 progress.
               </Typography>
             </Box>
-            <br />
           </Grid>
-          <Box
-            sx={{
-              padding: '1px'
-            }}
-          >
-            <Typography
-              variant="h4"
-              color="primary"
-              sx={{ padding: '3px', textAlign: 'center', marginLeft: '60px' }}
-            >
-              Lets collaborate, innovate, and build a better future together to
-              transform client experience, execute with excellence and drive
-              sustainable growth & value. - Together It Can Be!”
-            </Typography>
-          </Box>
         </Grid>
       </Container>
+      <Typography
+        variant="h4"
+        color="primary"
+        sx={{
+          padding: '3px',
+          textAlign: 'center',
+          marginLeft: '60px',
+          marginTop: '20px'
+        }}
+      >
+        Lets collaborate, innovate, and build a better future together to
+        transform client experience, execute with excellence, and drive
+        sustainable growth & value - Together It Can Be!
+      </Typography>
     </div>
   );
 }
